55a45b4fa44069e53afe790dd5d1dc437e2ff82a
retsaftu <tkazdayev@gmail.com>
2022-02-19 01:25:05 +0600

    connect to db add add and delete method

--- social-network/src/app/friends/friends.component.ts
+++ social-network/src/app/friends/friends.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-friends',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent implements OnInit {
-
+  friends: any;
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    //http://localhost:3000/auth/users
+    this.friends = (await axios({
+      method: 'get',
+      url: `http://localhost:3000/auth/users`,
+    })).data;
+  }
+  async add(friend: any) {
+    console.log(friend);
+
+  }
+  async delete(friend: any) {
+    console.log(friend);
+
   }
 
 }
