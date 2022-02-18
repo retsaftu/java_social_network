979bf8de0b77339b9f74aab24e939eb3eaa04c6a
Temir Mendigali <mendigalitemir13@gmail.com>
2022-02-19 02:36:58 +0600

    Add friends on both sides

--- backend/src/main/java/com/example/social_network/controller/FriendController.java
+++ backend/src/main/java/com/example/social_network/controller/FriendController.java
@@ -20,12 +20,16 @@ public class FriendController {
     public ResponseEntity<Users> addFriend(@PathVariable String userId, @RequestBody Friend friend) {
         try {
             System.out.println(friend.getName());
-            Users user = userRepository.findById(userId).orElse(null);
-            if (user != null) {
-                user.addFriend(friend);
-                userRepository.save(user);
+            Users user1 = userRepository.findById(userId).orElse(null);
+            Users user2 = userRepository.findById(friend.getUserId()).orElse(null);
+            if (user1 != null && user2 != null) {
+                user1.addFriend(friend);
+                userRepository.save(user1);
+                Friend friend1 = new Friend(user1.getName(), user1.get_id());
+                user2.addFriend(friend1);
+                userRepository.save(user2);
             }
-            return new ResponseEntity<>(user, HttpStatus.OK);
+            return new ResponseEntity<>(user1, HttpStatus.OK);
         } catch (Exception e) {
             System.out.println(e);
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
