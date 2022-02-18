5bb2e627c9af57dc253693a2ad3a30a826e60238
retsaftu <tkazdayev@gmail.com>
2022-02-18 06:08:58 +0600

    add header and edit feed

--- social-network/src/app/app.module.ts
+++ social-network/src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FeedComponent } from './feed/feed.component';
 import { MatCardModule } from '@angular/material/card';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeedComponent
+    FeedComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- social-network/src/app/feed/feed.component.ts
+++ social-network/src/app/feed/feed.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FeedComponent implements OnInit {
 
   constructor() { }
+  items = Array.from({ length: 100000 }).map((_, i) => `Item #${i}`);
 
   ngOnInit(): void {
   }
