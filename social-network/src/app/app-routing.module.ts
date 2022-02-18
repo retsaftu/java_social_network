e9cbd90dc60261342feb9ee06f91149f6f80b4ff
retsaftu <tkazdayev@gmail.com>
2022-02-18 06:59:36 +0600

    add friends page

--- social-network/src/app/app-routing.module.ts
+++ social-network/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './feed/feed.component'
+import { FriendsComponent } from './friends/friends.component'
 const routes: Routes = [
   { path: "", component: FeedComponent },
+  { path: "friends", component: FriendsComponent },
+
 ];
 
 @NgModule({
